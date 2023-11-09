02fb9193d031aaac9b20e345595fc57cbe3ec8cf
Igor Manoel <igorssmanoel@gmail.com>
2023-11-08 21:05:01 -0300

    Input example

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import { useState } from "react";
 
 const App = () => {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
     return <div>
+        <button onClick={() => setCount(count - 1)}>-</button>
         <button onClick={() => setCount(count + 1)}>+</button>
         <span>Contagem:</span> {count}
+        <div>Digite o nome: </div>
+        <input onChange={(event) => setName(event.target.value)} ></input>
+        <div>Meu nome é : {name}</div>
     </div>
 }
 
