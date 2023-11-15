c1969797681df8b50b054267e66db61217bd5fb2
Igor Manoel <igorssmanoel@gmail.com>
2023-11-15 20:29:57 -0300

    Exercicio calculadora

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
-    const [count, setCount] = useState(0)
-    const [name, setName] = useState("")
+    const [number, setNumber] = useState();
+    const multNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const gerarBotoes = () => {
+        return <div>
+            <button onClick={() => setNumber(1)}>1</button>
+            <button onClick={() => setNumber(2)}>2</button>
+            <button>3</button>
+            <br />
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <br />
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+        </div>
+    }
     return <div>
-        <button onClick={() => setCount(count - 1)}>-</button>
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <span>Contagem:</span> {count}
-        <div>Digite o nome: </div>
-        <input onChange={(event) => setName(event.target.value)} ></input>
-        <div>Meu nome é : {name}</div>
+        {/* <input
+            value={number}
+            onChange={(event) => setNumber(event.target.value)}
+            placeholder="Digite um número"></input> */}
+
+        {gerarBotoes()}
+        {number && multNumbers.map(item => <div>{item} X {number} = {item * number}</div>)}
     </div>
 }
 
